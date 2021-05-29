a45ff7b02ffda758f157c218226bbb7d60b8476e
Pam Willenz <pam@positek.net>
2021-05-29 15:53:30 -0400

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
 import "./App.css";
-import restaurant from "./restaurant.jpeg";
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
-}
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      <img src={restaurant} alt="main restaurant area" />
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
+function SecretComponent() {
+  return <h1>Secret information for authorized users only</h1>;
 }
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
+function RegularComponent() {
+  return <h1>Everyone can see this component.</h1>;
 }
 
-const dishes = [
-  "Macroni and Cheese",
-  "Salmon",
-  "Chicken Thighs",
-  "Tofu with Vegetables",
-];
-
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Pam" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+function App({ authorized }) {
+  return <>{authorized ? <SecretComponent /> : <RegularComponent />}</>;
 }
 
 export default App;
